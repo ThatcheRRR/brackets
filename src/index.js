4905067865315e4861e6f0b202b1557a1d53951d
ThatcheRRR <pavelosipov777@mail.ru>
2019-10-12 02:00:52 +0300

    feat: add taask solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = str.split(''),
+      leftBr = [],
+      rightBr = [];
+  if (brackets.length % 2 != 0) {
+      return false;
+  } else if (brackets.length % 2 == 0) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      leftBr.push(bracketsConfig[i][0]);
+      rightBr.push(bracketsConfig[i][1]);
+  }
+  for (let i = brackets.length - 1; i >= 0; i--) {
+      for (let j = 0; j < rightBr.length; j++) {
+          if (brackets[i] == leftBr[j] && brackets[i + 1] == rightBr[j]) {
+              brackets.splice(i, 2);
+          }
+      }
+  }
+  return (brackets.length == 0); 
+  }
+};
